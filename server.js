b1fa1cf4b028d70aaefbb8c97e2d42cb3b98ae7a
Omar Hadary <Omar Hadary>
2017-02-15 00:26:01 -0800

    mongo article and note models created. index.handlebars general layout created. server.js scappring and sotring news articles in db.

--- server.js
+++ server.js
@@ -42,6 +42,73 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+// ======
+
+// Simple index route
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+// A GET request to scrape the npr website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.npr.org/sections/news/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("h2.title").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
 // Listen on PORT
 app.listen(PORT, function() {
   console.log("App running on port " + PORT);
